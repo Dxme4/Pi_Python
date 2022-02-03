0ab748797a784e159b6fd53c98051e902d729ce1
Dxme4 <dimitarnorway@gmail.com>
2022-02-03 10:29:43 +0100

    da

--- FC-51.py
+++ FC-51.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO 
 import time 
+import LCD_I2C
 
  
 
--- LCD_I2C.py
+++ LCD_I2C.py
@@ -1,6 +1,11 @@
+import py_compile
 import I2C_LCD_driver
 import time
 
+
+
 mylcd = I2C_LCD_driver.lcd()
 
-mylcd.lcd_display_string("Hello World!", 2)
+mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string_pos("Dxme", 2, 0)
+
